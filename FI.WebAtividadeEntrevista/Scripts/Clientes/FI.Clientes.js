3b8a5a3b7b278c9eb47c8e6d8d1756005ea4746e
LorenaSavila <lorena.s.avila@outlook.com.com>
2023-03-29 00:08:00 -0300

    modal beneficiario

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
@@ -50,24 +50,38 @@ function TestaCPF(document) {
     var Resto;
     Soma = 0;
     var strCPF = document.value.replace(/[^0-9]/g, '');
-    console.log('aqwe');
-    if (strCPF == "00000000000") return ModalDialog("Ocorreu um erro", "CPF inválido.");
+
+    if (strCPF == "00000000000") return false;
 
     for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
     Resto = (Soma * 10) % 11;
 
     if ((Resto == 10) || (Resto == 11)) Resto = 0;
-    if (Resto != parseInt(strCPF.substring(9, 10))) return ModalDialog("Ocorreu um erro", "CPF inválido.");;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
 
     Soma = 0;
     for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
     Resto = (Soma * 10) % 11;
 
     if ((Resto == 10) || (Resto == 11)) Resto = 0;
-    if (Resto != parseInt(strCPF.substring(10, 11))) return ModalDialog("Ocorreu um erro", "CPF inválido.");;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
     return true;
 }
 
+function helpText(field, input) {
+  
+    var resultValid = TestaCPF(field);
+    var CPFfield = document.getElementById(input);
+
+    if (!resultValid) {
+        CPFfield.classList.add("text-danger")
+        CPFfield.innerHTML = "CPF inválido.";
+        return;
+    }
+
+    CPFfield.classList.remove("text-danger")
+    CPFfield.innerHTML = "";
+}
 
 function ModalDialog(titulo, texto) {
     var random = Math.random().toString().replace('.', '');
@@ -87,8 +101,54 @@ function ModalDialog(titulo, texto) {
         '                </div>                                                                                             ' +
         '            </div><!-- /.modal-content -->                                                                         ' +
         '  </div><!-- /.modal-dialog -->                                                                                    ' +
-        '</div> <!-- /.modal -->                                                                                        ';
+        '</div> <!-- /.modal -->                                                                                            ';
 
     $('body').append(texto);
+    $('#' + random).modal('show');
+}
+
+function ModalBeneficiario() {
+    var random = Math.random().toString().replace('.', '');
+    var modal = `
+        <div id=${random} class="modal fade">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                            <h4 class="modal-title"> Beneficiários </h4>
+                        </div>
+                        <div class="modal-body">
+                                <div class="row">
+                                     <div class="col-md-4">
+                                           <div class="form-group">
+                                                <label for="CPF">CPF:</label>
+                                                <input required="required" onblur="helpText(this, 'CPFBeneficiarioError')" onkeypress="format(this)" type="text" class="form-control" id="CPFBeneficioario" name="CPF" placeholder="Ex.: 010.011.010-10" maxlength="14">
+                                              <div id="CPFBeneficiarioError"> </div>
+                                            </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label for="Nome">Nome:</label>
+                                            <input required="required" type="text" class="form-control" id="NomeBeneficiario" name="Nome" placeholder="Ex.: João" maxlength="50">
+                                        </div>
+                                    </div>
+                                    <div class="pull-left">
+                                      <div class="form-group">
+                                        <button type="submit" class="btn btn-sm btn-success">Incluir</button>
+                                      </div>
+                                     </div>
+                                </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+
+                        </div>
+                    </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div> <!-- /.modal -->
+    
+    `
+
+    $('body').append(modal);
     $('#' + random).modal('show');
 }
